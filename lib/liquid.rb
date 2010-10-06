fd8455d7a2d382d6e81eb7ad0599d1322cc78c30
Jay Zeschin <jay@zeschin.org>
2010-10-05 22:05:08 -0700

    Require liquid/tags/block first

--- lib/liquid.rb
+++ lib/liquid.rb
@@ -64,6 +64,8 @@ require 'liquid/module_ex'
 
 # Load all the tags of the standard library
 #
+require File.dirname(__FILE__) + '/liquid/tags/block.rb'
+
 Dir[File.dirname(__FILE__) + '/liquid/tags/*.rb'].each { |f| require f }
 
 # Load Liquid::Document, which until revision inherits from Block, thus is a Liquid::Tag
